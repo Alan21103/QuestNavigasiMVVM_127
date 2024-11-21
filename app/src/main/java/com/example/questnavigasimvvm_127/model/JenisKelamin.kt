d629b070555e62162b50d428d530651864253ebe
Alan21103 <syahlansaputra03@gmail.com>
2024-11-21 22:15:32 +0700

    Membuat object jenis kelamin

---
+++ app/src/main/java/com/example/questnavigasimvvm_127/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasimvvm_127.model
+
+import com.example.questnavigasimvvm_127.R
+
+object JenisKelamin {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
