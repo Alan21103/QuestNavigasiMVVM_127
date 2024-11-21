d972fb266d343e0cfbb2b98c851046571e9b79b6
Alan21103 <syahlansaputra03@gmail.com>
2024-11-21 21:57:41 +0700

    Membuat fungsi Formulir view dengan menginputkan string dari data siswa

---
+++ app/src/main/java/com/example/questnavigasimvvm_127/ui/view/FormulirView.kt
@@ -0,0 +1,22 @@
+package com.example.questnavigasimvvm_127.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormulirView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
+) {
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var notelepon by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var nim by remember { mutableStateOf("") }
+}
