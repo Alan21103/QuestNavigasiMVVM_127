94563554c204e9015b7026654fc443125088fb76
Alan21103 <syahlansaputra03@gmail.com>
2024-11-21 22:00:32 +0700

    Membuat Placeholder untuk menginputkan nama

--- app/src/main/java/com/example/questnavigasimvvm_127/ui/view/FormulirView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_127/ui/view/FormulirView.kt
@@ -1,11 +1,19 @@
 package com.example.questnavigasimvvm_127.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirView(
@@ -19,4 +27,24 @@ fun FormulirView(
     var notelepon by remember { mutableStateOf("") }
     var gender by remember { mutableStateOf("") }
     var nim by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(nama, alamat, gender, notelepon, email, nim)
+
+    Column ( modifier = modifier
+        .fillMaxSize()
+        .padding(top = 30.dp)
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        TextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = {
+                Text(text = "Nama")
+            },
+            placeholder = {
+                Text(text = "Isi Nama Anda")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
