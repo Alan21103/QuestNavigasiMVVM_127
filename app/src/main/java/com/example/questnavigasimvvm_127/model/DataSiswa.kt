f06e09362aa8e4d05cd7b6c17ee6a691c0adb760
Alan21103 <syahlansaputra03@gmail.com>
2024-11-21 22:15:17 +0700

    Membuat Datasiswa

---
+++ app/src/main/java/com/example/questnavigasimvvm_127/model/DataSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasimvvm_127.model
+
+data class DataSiswa(
+    val nama: String = "",
+    val alamat: String = "",
+    val gender: String = "",
+    val notelepon: String = "",
+    val email: String = "",
+    val nim: String = ""
+)
